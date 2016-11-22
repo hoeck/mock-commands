8f71bfa0db1632c73c69cfb64c6ff614267893fc
hoeck <eriksoehnel@googlemail.com>
2016-11-22 07:31:03 +0100

    First tests and API tuning

--- test/example.spec.js
+++ test/example.spec.js
@@ -5,15 +5,111 @@ const MockScriptEnvironment = require('..');
 describe('mock-script-environment', () => {
     const scriptEnv = new MockScriptEnvironment({path: '/test'});
 
-    describe('".prepareFiles"', () => {
+    describe('method "getWorkDir"', () => {
+        it('should return the working directory', () => {
+            expect(scriptEnv.getWorkdir()).toBe('/test/workdir');
+        });
+    });
+
+    describe('method "prepareFiles"', () => {
+        const workdir = scriptEnv.getWorkdir();
+
         afterEach(() => {
             scriptEnv.clear();
         });
 
-        it('should write the specified files to "/test/workdir"', () => {
+        it('should write the specified files to the working directory', () => {
             scriptEnv.prepareFiles({'foo.txt': 'a\nb\n', 'bar.txt': '123'});
 
-            expect(fs.readFileSync('/test/workdir/foo.txt').toString()).toBe('a\nb\n');
+            expect(fs.readFileSync(`${workdir}/foo.txt`).toString()).toBe('a\nb\n');
+            expect(fs.readdirSync(`${workdir}`)).toEqual(['bar.txt', 'foo.txt']);
         });
+
+        it('should be able to create files in nested directories', () => {
+            scriptEnv.prepareFiles({'foo/bar': '1', 'foo/foo/bar': '2'});
+
+            expect(fs.readFileSync(`${workdir}/foo/bar`).toString()).toBe('1');
+            expect(fs.readFileSync(`${workdir}/foo/foo/bar`).toString()).toBe('2');
+        });
+
+        it('should be idempotent', () => {
+            scriptEnv.prepareFiles({foo: '1', baz: '2'});
+            scriptEnv.prepareFiles({foo: '1', bar: '3'});
+
+            expect(fs.readFileSync(`${workdir}/foo`).toString()).toBe('1');
+            expect(fs.readFileSync(`${workdir}/bar`).toString()).toBe('3');
+            expect(fs.readFileSync(`${workdir}/baz`).toString()).toBe('2');
+
+            expect(fs.readdirSync(workdir)).toEqual(['bar', 'baz', 'foo']);
+        });
+    });
+
+    describe('method "mockCommand"', () => {
+        afterEach(() => {
+            scriptEnv.clear();
+        });
+
+        it('should setup an executable script using the given spy', (done) => {
+            scriptEnv.mockCommand('foo', () => 'foo-stdout');
+
+            scriptEnv.exec('foo').then((res) => {
+                expect(res.stdout).toBe('foo-stdout');
+                expect(res.stderr).toBe('');
+                expect(res.exitCode).toBe(0);
+
+                done();
+            }).catch(done.fail);
+        });
+
+        it('should provide an extended API to control return codes and stderr output', (done) => {
+            scriptEnv.mockCommand('foo', () => ({
+                stdout: 'foo-stdout',
+                stderr: 'foo-stderr',
+                exitCode: 13
+            }));
+
+            scriptEnv.exec('foo').then(done.fail).catch((err) => {
+                expect(err.exitCode).toBe(13);
+                expect(err.stdout).toBe('foo-stdout');
+                expect(err.stderr).toBe('foo-stderr');
+
+                done();
+            });
+        });
+    });
+
+    describe('method "clear"', () => {
+        it('should delete everything in the work dir', () => {
+            scriptEnv.prepareFiles({foo: 'bar'});
+            scriptEnv.clear();
+
+            expect(fs.existsSync(scriptEnv.getWorkdir())).toBe(true);
+            expect(fs.readdirSync(scriptEnv.getWorkdir())).toEqual([]);
+        });
+
+        it('should delete everything in the bin dir', () => {
+            scriptEnv.mockCommand('foo', () => { });
+            scriptEnv.clear();
+
+            expect(fs.existsSync('/test/bin')).toBe(false);
+        });
+
+        it('should reset all mock commands', () => {
+            scriptEnv.mockCommand('foo', () => { });
+
+            // redefinition of foo throws an error
+            expect(() => {
+                scriptEnv.mockCommand('foo', () => { });
+            }).toThrow();
+
+            scriptEnv.clear();
+
+            // now that all commands are cleared, redefinition of foo must be
+            // possible again
+            expect(() => {
+                scriptEnv.mockCommand('foo', () => { });
+            }).not.toThrow();
+        });
+
     });
 });
